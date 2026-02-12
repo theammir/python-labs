cc8b4559c5dab16d3ff5fd80b1ac4cdb09497a2b
theammir <the@ammir.dev>
2026-02-13 00:57:36 +0200

    chore(5): RAII except each `with` nests the code

---
+++ lab5/main.py
@@ -0,0 +1,22 @@
+# Загальне завдання
+# Написати програму, яка зчитує дані з текстового файлу та записує їх у новий
+# файл, виконуючи певні перетворення (наприклад, переведення всіх літер у
+# нижній регістр).
+# Індивідуальні завдання
+# 9) Видалити всі порожні рядки з файлу.
+
+import os
+import sys
+
+INPUT_PATH = "input.txt"
+OUTPUT_PATH = "output.txt"
+
+if not os.path.exists(INPUT_PATH):
+    print("The file does not exist:", INPUT_PATH)
+    sys.exit(1)
+
+output = open(OUTPUT_PATH, "w")
+with open(INPUT_PATH, "r") as f:
+    content = f.readlines()
+    output.writelines([line for line in content if line.strip() != ""])
+output.close()
