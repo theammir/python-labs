67fdf25e7bdbb6e209470cfdc9a8979e3f366bab
theammir <the@ammir.dev>
2026-02-13 00:31:10 +0200

    chore(3): i don't know what's the catch here honestly

---
+++ lab3/main.py
@@ -0,0 +1,11 @@
+# Загальне завдання
+# Написати програму, яка:
+# − вводить початкові дані списку/словника;
+# − обчислює результат;
+# − виводить результат.
+# Індивідуальні завдання
+# 9) Написати програму для об’єднання двох списків в один.
+
+array1 = input("Enter whatever,  separated by whitespace: ").split()
+array2 = input("Enter once more, separated by whitespace: ").split()
+print(array1 + array2)
