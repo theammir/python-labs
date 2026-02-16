92f8e532408703c4895380ab0a323398ebf3bbb2
theammir <the@ammir.dev>
2026-02-16 02:52:19 +0200

    chore(9): it was actually kind of fun

---
+++ lab9/main.py
@@ -0,0 +1,21 @@
+# 9.7 Загальне завдання
+# Написати програму, яка:
+# − вводить початкові дані (з файлу або з клавіатури);
+# − формує результат з використанням регулярних виразів;
+# − виводить результат на екран або зберігає у файл.
+# Індивідуальні завдання
+# 9) Створити програму для перевірки, чи є рядок валідною IP-адресою.
+
+import re
+
+# I wrote it myself: https://regex101.com/r/4ZVtv0/1
+# Didn't give it *too* much thought, but it should correctly check
+# that octets are within 0-255, and there are no redundant zeroes
+IPV4_PATTERN = re.compile(
+    r"^((([0-9]|[1-9]{2})|([1-2][0-4][0-9])|(25[0-5]))\.){3}(([0-9]|[1-9]{2})|([1-2][0-4][0-9])|(25[0-5]))$"
+)
+
+if re.match(IPV4_PATTERN, input("Enter an IPv4 address: ").strip()):
+    print("This is a valid IPv4 address.")
+else:
+    print("This is NOT a valid IPv4 address.")
