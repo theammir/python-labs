f31fbda6145be58cb181afe2c729767012cb7887
theammir <the@ammir.dev>
2026-02-16 12:13:55 +0200

    chore(7): output to multiple streams, including a log file

--- lab7/main.py
+++ lab7/main.py
@@ -6,23 +6,32 @@
 # Індивідуальні завдання
 # 9) Створити програму, яка зчитує дані з файлу та перевіряє їх відповідність
 # заданому формату.
+
 import sys
+from typing import Iterable, TextIO
 
 # INPUT_PATH = "input.txt"
 INPUT_PATH = "input_nonexistent.txt"
+LOG_FILE = open("log.txt", "w")
+
+
+def log(line: str, streams: Iterable[TextIO] = (LOG_FILE, sys.stdout)) -> None:
+    for stream in streams:
+        stream.write(line + "\n")
+
 
 content: str
 try:
     f = open(INPUT_PATH, "r", encoding="utf-8")
     content = f.read().strip()
 except IOError as e:
-    print("OS error occured while reading the file:", e)
+    log(f"OS error occured while reading the file: {e}")
     sys.exit(1)
 except Exception as e:
-    print(f"Unknown error occured: {e.__class__.__name__}: {e}")
+    log(f"Unknown error occured: {e.__class__.__name__}: {e}")
     sys.exit(1)
 
 if content.isnumeric():
-    print("The file only contains a number.")
+    log("The file only contains a number.")
 else:
-    print("The file does not only contain a number.")
+    log("The file does not only contain a number.")
